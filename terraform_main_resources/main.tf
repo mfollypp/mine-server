b8aaa98ab6e81d15c0366ed7b45224197b398660
Matheus Folly <matheusfolly@id.uff.br>
2024-01-18 11:29:51 -0300

    uncommenting azure vnet and subnet for container instance

--- terraform_main_resources/main.tf
+++ terraform_main_resources/main.tf
@@ -55,19 +55,19 @@ resource "azurerm_container_group" "mine_server" {
   }
 }
 
-# resource "azurerm_virtual_network" "mine_server_vnet" {
-#   name                = "mineserver-vnet"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = data.azurerm_resource_group.mine_server_rg.location
-#   resource_group_name = data.azurerm_resource_group.mine_server_rg.name
-# }
+resource "azurerm_virtual_network" "mine_server_vnet" {
+  name                = "mineserver-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.mine_server_rg.location
+  resource_group_name = data.azurerm_resource_group.mine_server_rg.name
+}
 
-# resource "azurerm_subnet" "mine_server_subnet" {
-#   name                 = "mineserver-subnet"
-#   resource_group_name  = data.azurerm_resource_group.mine_server_rg.name
-#   virtual_network_name = azurerm_virtual_network.mine_server_vnet.name
-#   address_prefixes     = ["10.0.1.0/24"]
-# }
+resource "azurerm_subnet" "mine_server_subnet" {
+  name                 = "mineserver-subnet"
+  resource_group_name  = data.azurerm_resource_group.mine_server_rg.name
+  virtual_network_name = azurerm_virtual_network.mine_server_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
 # resource "azurerm_subnet" "gateway_subnet" {
 #   name                 = "mineserver-gateway-subnet"
