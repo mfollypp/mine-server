3fd7e20e1f16669bec474741228a4f0f67198ae3
Matheus Folly <matheusfolly@id.uff.br>
2024-01-19 00:39:04 -0300

    removing dns label reuse policy and destroying public ip resource previously created

--- terraform_main_resources/main.tf
+++ terraform_main_resources/main.tf
@@ -14,19 +14,18 @@ resource "azurerm_storage_share" "mine_file_share" {
 }
 
 resource "azurerm_container_group" "mine_server" {
-  name                        = "mine-server"
-  location                    = data.azurerm_resource_group.mine_server_rg.location
-  resource_group_name         = data.azurerm_resource_group.mine_server_rg.name
-  ip_address_type             = "Public"
-  os_type                     = "Linux"
-  dns_name_label              = "folly-mine-server"
-  dns_name_label_reuse_policy = "Noreuse"
+  name                = "mine-server"
+  location            = data.azurerm_resource_group.mine_server_rg.location
+  resource_group_name = data.azurerm_resource_group.mine_server_rg.name
+  ip_address_type     = "Public"
+  os_type             = "Linux"
+  dns_name_label      = "folly-mine-server"
 
   container {
     name                  = "mine-container"
     image                 = "itzg/minecraft-server:latest"
-    cpu                   = "4"
-    memory                = "8"
+    cpu                   = "3"
+    memory                = "6"
     environment_variables = { "EULA" = "true" }
 
     volume {
@@ -49,31 +48,3 @@ resource "azurerm_container_group" "mine_server" {
     }
   }
 }
-
-resource "azurerm_public_ip" "mine_lb_public_ip" {
-  name                = "mine-lb-public-ip"
-  resource_group_name = data.azurerm_resource_group.mine_server_rg.name
-  allocation_method   = "Static"
-  location            = data.azurerm_resource_group.mine_server_rg.location
-}
-
-# resource "azurerm_lb" "mine_lb" {
-#   name                = "mine-lb"
-#   resource_group_name = data.azurerm_resource_group.mine_server_rg.name
-#   location            = data.azurerm_resource_group.mine_server_rg.location
-#   sku                 = "Basic"
-
-#   frontend_ip_configuration {
-#     name                 = "PublicIPAddress"
-#     public_ip_address_id = azurerm_public_ip.mine_lb_public_ip.id
-#   }
-# }
-
-# resource "azurerm_lb_rule" "mine_lb_rule" {
-#   name                  = "mine-lb-rule"
-#   loadbalancer_id       = azurerm_lb.mine_lb.id
-#   protocol              = "Tcp"
-#   frontend_port         = 25565
-#   backend_port          = 25565
-#   frontend_ip_configuration_name = "PublicIPAddress"
-# }
